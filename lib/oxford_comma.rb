16bd7eca3a9354571cbc1b041c5a762f0c16f38f
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-23 22:55:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   when 2
     return array.join(" and ")
   when (3..)
-    array[-1] = "and #{array[-1]}"
+    array.last = "and #{array[-1]}"
     return array.join(", ")
  end
 end
