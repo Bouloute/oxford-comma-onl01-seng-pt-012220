8eb7665c15664c9df78ef2dc7419b967b73f2678
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-23 22:54:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,16 +9,3 @@ def oxford_comma(array)
     return array.join(", ")
  end
 end
-
-
-#find the last element. ake an array isolating the last. join with an " and "
-#new_last_array_item = "and #{array[-1]}"#
-#    array.pop
-#    array.push(new_last_array_item)
-#    return array.join(", ")
-
-
-
-
-#  array[-1] = "and #{array[-1]}"
-#  return array.join(", ")
